9d044f556600044f5a412a609a7b9cd17c042b86
zouhangte <zouhangtezbm@126.com>
2016-11-28 19:00:12 +0800

    添加dotween文件

---
+++ Assets/NaviCtrl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+public interface INaviCtrl
+{
+    bool ReStartNavi(out NaviItem item);
+    void StopNavi();
+    bool NextNavi(out NaviItem item);
+}
+
+public class NaviCtrl : INaviCtrl
+{
+    public int index;
+    public IList<NaviItem> naviList;
+    private bool stop;
+    public NaviCtrl(List<NaviItem> naviList)
+    {
+        this.naviList = naviList;
+    }
+
+    public bool NextNavi(out NaviItem item)
+    {
+        if (stop)
+        {
+            item = null;
+            return false;
+        }
+        if (index < naviList.Count)
+        {
+            item = naviList[index];
+            index++;
+            return true;
+        }
+        else
+        {
+            //StopNavi();
+            item = null;
+            return false;
+        }
+    }
+
+    public bool ReStartNavi(out NaviItem item)
+    {
+        stop = false;
+         index = 0;
+        return NextNavi(out item);
+    }
+
+    public void StopNavi()
+    {
+        stop = true;
+    }
+}
---
+++ Assets/Scripts/RectRechange.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class RectRechange : MonoBehaviour {
+    public enum PosID
+    {
+        ru,
+        rd,
+        lu,
+        ld
+    }
+
+
+    public Transform ru;
+    public Transform rd;
+    public Transform lu;
+    public Transform ld;
+
+    public RectTransform item;
+    private Transform temp;
+
+    public void ResetItemPos(PosID posid)
+    {
+        switch (posid)
+        {
+            case PosID.ru:
+                temp = ru.GetChild(0);
+                SetActive(rubool:true);
+                SetLeftCenter();
+                break;
+            case PosID.rd:
+                temp = rd.GetChild(0);
+                SetActive(rdbool: true);
+                SetLeftCenter();
+                break;
+            case PosID.lu:
+                temp = lu.GetChild(0);
+                SetActive(lubool: true);
+                SetRightCenter();
+                break;
+            case PosID.ld:
+                temp = ld.GetChild(0);
+                SetActive(ldbool: true);
+                SetRightCenter();
+                break;
+            default:
+                break;
+        }
+        item.SetParent(temp,false);
+        item.transform.localPosition = Vector3.zero;
+    }
+
+    void SetLeftCenter()
+    {
+        item.pivot =new Vector2(0, 0.5f);
+    }
+
+    void SetRightCenter()
+    {
+        item.pivot = new Vector2(1, 0.5f);
+    }
+    void SetActive(bool rubool = false,bool rdbool = false,bool lubool = false,bool ldbool= false)
+    {
+        ru.gameObject.SetActive(rubool);
+        rd.gameObject.SetActive(rdbool);
+        lu.gameObject.SetActive(lubool);
+        ld.gameObject.SetActive(ldbool);
+    }
+}
