a1532674dc0f424c435c343663e1570b23e1dbf3
zouhangte <zouhangtezbm@126.com>
2017-06-23 10:11:11 +0800

    add demo learn from http://www.xuanyusong.com/archives/4408

---
+++ Assets/NaviSystem/Script/NaviNode.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+
+public class NaviNode : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler,IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public UnityEngine.Events.UnityAction onComplete;
+
+    //监听按下
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        PassEvent(eventData, ExecuteEvents.pointerDownHandler);
+    }
+
+    //监听抬起
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        PassEvent(eventData, ExecuteEvents.pointerUpHandler);
+    }
+
+    //监听点击
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PassEvent(eventData, ExecuteEvents.submitHandler);
+        PassEvent(eventData, ExecuteEvents.pointerClickHandler);
+        onComplete.Invoke();
+    }
+    //鼠标移入
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        PassEvent(eventData, ExecuteEvents.pointerEnterHandler);
+    }
+    //鼠标移出 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        PassEvent(eventData, ExecuteEvents.pointerExitHandler);
+    }
+
+    //把事件透下去
+    public void PassEvent<T>(PointerEventData data, ExecuteEvents.EventFunction<T> function)
+        where T : IEventSystemHandler
+    {
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(data, results);
+        GameObject current = data.pointerCurrentRaycast.gameObject;
+        for (int i = 0; i < results.Count; i++)
+        {
+            if (current != results[i].gameObject)
+            {
+                ExecuteEvents.Execute(results[i].gameObject, data, function);
+                //RaycastAll后ugui会自己排序，如果你只想响应透下去的最近的一个响应，这里ExecuteEvents.Execute后直接break就行。
+            }
+        }
+    }
+
+    
+}
---
+++ Assets/NaviSystem/Script/NaviPanel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class NaviPanel : MonoBehaviour {
+    public NaviMask navimask;
+    public List<NaviNode> naviNodes;
+    private int _id = -1;
+    private NaviNode node;
+    private void Start()
+    {
+        if (naviNodes == null || navimask == null) Debug.LogError("[emptyerr]:naviNodes or navimask is null");
+        foreach (var item in naviNodes)
+        {
+            item.onComplete = NextNavi;
+        }
+        NextNavi();
+    }
+
+    private void NextNavi()
+    {
+        _id++;
+        if (_id == naviNodes.Count)
+        {
+            OnComplete();
+        }
+        else
+        {
+            ActiveStep();
+        }
+    }
+
+    private void ActiveStep()
+    {
+        for (int i = 0; i < naviNodes.Count; i++)
+        {
+            naviNodes[i].gameObject.SetActive(i == _id);
+            if (i== _id)
+            {
+                navimask.Restart(naviNodes[i]);
+            }
+        }
+    }
+
+    private void OnComplete()
+    {
+        Destroy(gameObject);
+    }
+}
